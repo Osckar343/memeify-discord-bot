e4045f04c41d8e770f3300080e09ae3d17537769
Oscar Mendez <oscarandres131@hotmail.com>
2021-06-20 14:13:05 -0500

    0.5.6, messages are checked by their regular expressions, not by file names

--- answers/a.js
+++ answers/a.js
@@ -1,8 +1,8 @@
 module.exports = {
     name: 'a',
     description: 'Responses with Hey',
+    expression: /\b[aA]\b/is,
     execute(message) {
         message.lineReply('Hey'); 
-        message.lineReplyNoMention(`My name is`);
     }
 }
